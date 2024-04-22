8693214ec2bf9507b9d85aa4015b880fdfcacd6c
venkydasari <37236750+venkydasari@users.noreply.github.com>
2024-04-22 13:15:01 -0700

    special variables

--- shell-script/08-specialvariable.sh
+++ shell-script/08-specialvariable.sh
@@ -2,11 +2,11 @@
 
 echo " print all variavles $@ "
 echo " print no of variable $# "
-#echo "script name $0 "
-#echo " print user name $USER "
-#echo " current working directory $PWD "
-#echo " present home directory $HOME "
-#echo " host name is $HOSTNAME "
-#echo " process id of current shell script $$ "
+echo "script name $0 "
+echo " print user name $USER "
+echo " current working directory $PWD "
+echo " present home directory $HOME "
+echo " host name is $HOSTNAME "
+echo " process id of current shell script $$ "
 #sleep 30 $
 #echo " process id of last backgroung id $!"
